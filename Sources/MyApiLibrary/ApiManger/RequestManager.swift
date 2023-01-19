9e3aca2593ffb93b0bc5e3bda8a58fa80760a4f6
Mudassar Ahmad <mudassar.ahmad@starzplayarabia.com>
2023-01-19 23:04:55 +0500

    Add network check before the class

--- Sources/MyApiLibrary/ApiManger/RequestManager.swift
+++ Sources/MyApiLibrary/ApiManger/RequestManager.swift
@@ -12,34 +12,37 @@ public protocol RequestManager{
 }
 extension RequestManager {
     public  func sendApiRequest<T: Decodable>(request: Request, responseModel: T.Type) async -> Result<T, RequestError> {
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request.urlRequest, delegate: nil)
-            guard let response = response as? HTTPURLResponse else {
-                return .failure(.noResponse)
-            }
-
-            switch response.statusCode {
-            case 200...299:
-                do {
-                    let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
-                    return .success(decodedResponse)
-
-                } catch let error {
-                    print(error)
-                    return .failure(.decode)
+        if NetworkMonitor.shared.isConnected {
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request.urlRequest, delegate: nil)
+                guard let response = response as? HTTPURLResponse else {
+                    return .failure(.noResponse)
                 }
-
-            case 401:
-                let error = try parseJSON(from: data)
-                return .failure(.unauthorized(reason: error.statusMessage))
-
-            default:
-                return .failure(.unexpectedStatusCode)
+                
+                switch response.statusCode {
+                case 200...299:
+                    do {
+                        let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
+                        return .success(decodedResponse)
+                        
+                    } catch let error {
+                        print(error)
+                        return .failure(.decode)
+                    }
+                    
+                case 401:
+                    let error = try parseJSON(from: data)
+                    return .failure(.unauthorized(reason: error.statusMessage))
+                    
+                default:
+                    return .failure(.unexpectedStatusCode)
+                }
+            } catch {
+                return .failure(.unknown)
             }
-        } catch {
-            return .failure(.unknown)
+        } else {
+            return .failure(.noInternet)
         }
-
     }
 
 }
--- Sources/MyApiLibrary/Base/ClientManager.swift
+++ Sources/MyApiLibrary/Base/ClientManager.swift
@@ -11,61 +11,62 @@ public protocol NetworkManagerService {
     func sendApiRequest<T: Decodable>(endpoint: ApiEndpoint, responseModel: T.Type) async -> Result<T, RequestError>
 }
 extension NetworkManagerService {
-  public  func sendApiRequest<T: Decodable>(endpoint: ApiEndpoint, responseModel: T.Type) async -> Result<T, RequestError> {
-
-        var urlComponents = URLComponents()
-        urlComponents.scheme = endpoint.scheme
-        urlComponents.host = endpoint.host
-        urlComponents.path = endpoint.path
-        urlComponents.queryItems = endpoint.queryItems
-
-        guard let url = urlComponents.url else {
-            return .failure(.invalidURL)
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = endpoint.method.rawValue
-        request.allHTTPHeaderFields = endpoint.header
-
-        if let body = endpoint.body {
+    public  func sendApiRequest<T: Decodable>(endpoint: ApiEndpoint, responseModel: T.Type) async -> Result<T, RequestError> {
+        if NetworkMonitor.shared.isConnected {
+            var urlComponents = URLComponents()
+            urlComponents.scheme = endpoint.scheme
+            urlComponents.host = endpoint.host
+            urlComponents.path = endpoint.path
+            urlComponents.queryItems = endpoint.queryItems
+            
+            guard let url = urlComponents.url else {
+                return .failure(.invalidURL)
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = endpoint.method.rawValue
+            request.allHTTPHeaderFields = endpoint.header
+            
+            if let body = endpoint.body {
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: body, options: [])
+                    request.httpBody = jsonData
+                } catch {
+                    return .failure(.unknown)
+                }
+            }
+            
             do {
-                let jsonData = try JSONSerialization.data(withJSONObject: body, options: [])
-                request.httpBody = jsonData
+                let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+                guard let response = response as? HTTPURLResponse else {
+                    return .failure(.noResponse)
+                }
+                
+                switch response.statusCode {
+                case 200...299:
+                    do {
+                        let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
+                        return .success(decodedResponse)
+                        
+                    } catch let error {
+                        print(error)
+                        return .failure(.decode)
+                    }
+                    
+                case 401:
+                    let error = try parseJSON(from: data)
+                    return .failure(.unauthorized(reason: error.statusMessage))
+                    
+                default:
+                    return .failure(.unexpectedStatusCode)
+                }
             } catch {
                 return .failure(.unknown)
             }
+        } else {
+            return .failure(.noInternet)
         }
-
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
-            guard let response = response as? HTTPURLResponse else {
-                return .failure(.noResponse)
-            }
-
-            switch response.statusCode {
-            case 200...299:
-                do {
-                    let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
-                    return .success(decodedResponse)
-
-                } catch let error {
-                    print(error)
-                    return .failure(.decode)
-                }
-
-            case 401:
-                let error = try parseJSON(from: data)
-                return .failure(.unauthorized(reason: error.statusMessage))
-
-            default:
-                return .failure(.unexpectedStatusCode)
-            }
-        } catch {
-            return .failure(.unknown)
-        }
-
     }
-
 }
 func parseJSON(from data: Data) throws -> GenricModel {
    do {
